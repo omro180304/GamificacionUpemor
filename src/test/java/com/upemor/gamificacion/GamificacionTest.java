d4d10fb55a1bcd66b8a2b6f5168858b0dcbecfbf
omro180304 <112585811+omro180304@users.noreply.github.com>
2022-08-31 16:16:27 -0500

    Add files via upload

---
+++ src/test/java/com/upemor/gamificacion/GamificacionTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.upemor.gamificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *
+ * @author Archivos Propios
+ */
+@SpringBootTest
+public class GamificacionTest {
+    @Test
+	void contextLoads() {
+	}
+}
